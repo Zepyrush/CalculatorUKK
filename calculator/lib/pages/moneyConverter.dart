6d45cd423903f6e50a6b269ee1cbf53ace17f49a
Zepyrush <p6817305788@gmail.com>
2024-03-06 09:27:47 +0700

    Create moneyConverter.dart
    Co-Authored-By: PangeranCoding <162389788+pangerancoding@users.noreply.github.com>

---
+++ calculator/lib/pages/moneyConverter.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class money extends StatelessWidget {
+  const money({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
